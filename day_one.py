4f0cfa9aad65d9bcc0a3d85d39a5abc01fef64fc
Joanna White <37188631+digitensions@users.noreply.github.com>
2022-12-01 17:14:35 +0000

    Create day_one.py

---
+++ day_one.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+elves = []
+cals = 0
+
+with open('day_one_input.txt', 'r') as treats:
+    deliciousness = treats.readlines()
+    for deli in deliciousness:
+        if deli.startswith(('\n', '\r\n')):
+            elves.append(cals)
+            cals = 0
+        else:
+            cals += int(deli)
+
+elves.sort()
+print(f"Biggest elf calories: {sort[-1]}")
